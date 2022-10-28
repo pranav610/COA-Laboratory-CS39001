731c53aae205940e6265d337fe65a144492e7f72
pranav610 <pranavkulkarni610@gmail.com>
2022-10-28 15:05:39 +0530

    diff.v working on tb

--- RISC/diff.v
+++ RISC/diff.v
@@ -19,17 +19,17 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 // module to perform the diff operation: i.e. to find the LSB bit where A and B differ
-module diff(A, B, diff, eq);
+module diff(A, B, diff, eq, cout);
 	input [31:0] A;
 	input [31:0] B;
 	output [5:0] diff;
 	output eq;
+	output cout;
   
 	wire [31:0] n, n_1, n_n_1_xor, diff_32;
-	wire cout1 ;
 	
 	assign n = A ^ B;
-	adder32 g1 (n, 32'b11111111111111111111111111111111, 1'b0, n_1, cout1);
+	adder32 g1 (n, 32'b11111111111111111111111111111111, 1'b0, n_1, cout);
 	assign n_n_1_xor = n ^ n_1;
 	assign diff_32 = n_n_1_xor & n;
 	
---
+++ RISC/isim/diff_tb_isim_beh.exe.sim/work/m_06977998105512429373_0311818195.c
@@ -0,0 +1,93 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0xfbc00daa */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "/home/pranav/KGP/5th Sem/COA LAB/COA-Laboratory-CS39001/RISC/diff_tb.v";
+static int ng1[] = {0, 0};
+static unsigned int ng2[] = {4294967295U, 0U};
+static unsigned int ng3[] = {4294967039U, 0U};
+static unsigned int ng4[] = {4294934271U, 0U};
+
+
+
+static void Initial_45_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+
+LAB0:    t1 = (t0 + 2840U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(45, ng0);
+
+LAB4:    xsi_set_current_line(47, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 32);
+    xsi_set_current_line(48, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 32);
+    xsi_set_current_line(51, ng0);
+    t2 = (t0 + 2648);
+    xsi_process_wait(t2, 10000LL);
+    *((char **)t1) = &&LAB5;
+
+LAB1:    return;
+LAB5:    xsi_set_current_line(52, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 32);
+    xsi_set_current_line(53, ng0);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 32);
+    xsi_set_current_line(55, ng0);
+    t2 = (t0 + 2648);
+    xsi_process_wait(t2, 10000LL);
+    *((char **)t1) = &&LAB6;
+    goto LAB1;
+
+LAB6:    xsi_set_current_line(56, ng0);
+    t2 = ((char*)((ng4)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 32);
+    xsi_set_current_line(57, ng0);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 32);
+    goto LAB1;
+
+}
+
+
+extern void work_m_06977998105512429373_0311818195_init()
+{
+	static char *pe[] = {(void *)Initial_45_0};
+	xsi_register_didat("work_m_06977998105512429373_0311818195", "isim/diff_tb_isim_beh.exe.sim/work/m_06977998105512429373_0311818195.didat");
+	xsi_register_executes(pe);
+}
